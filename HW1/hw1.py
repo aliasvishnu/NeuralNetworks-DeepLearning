c668180a46df57df80e6bc09847b8d39d4abc2c8
aliasvishnu <aliasvishnu@gmail.com>
2017-01-13 00:54:36 -0800

    Added data read portion

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -1,2 +1,16 @@
 import numpy as np
 import random
+
+# "pip install python-mnist"
+from mnist import MNIST
+mndata = MNIST('Dataset')
+trainX = np.array(mndata.load_training()[0])[:20000]
+trainY = np.array(mndata.load_training()[1])[:20000]
+testX = np.array(mndata.load_testing()[0])[:2000]
+testY = np.array(mndata.load_testing()[1])[:2000]
+
+# Logistic Regression Portion
+
+
+
+# Begin softmax here, do not change trainX, trainY, testY, testX
