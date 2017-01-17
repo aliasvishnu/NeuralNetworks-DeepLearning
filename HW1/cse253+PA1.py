213d81c0c5e56f92954d4e585bcaae9b67a55c32
ojasgupta <ogupta@ucsd.edu>
2017-01-17 12:42:02 -0800

    softmax

--- HW1/cse253+PA1.py
+++ HW1/cse253+PA1.py
@@ -1,14 +1,15 @@
 
 # coding: utf-8
 
-# In[12]:
+# In[1]:
 
 import numpy as np
 import random
 from mnist import MNIST
+import matplotlib.pyplot as plt
 
 
-# In[47]:
+# In[143]:
 
 mndata = MNIST('Dataset')
 trainX = np.array(mndata.load_training()[0])[:18000]
@@ -19,42 +20,58 @@ testX = np.array(mndata.load_testing()[0])[:2000]
 testY = np.array(mndata.load_testing()[1])[:2000]
 
 
-# In[49]:
+# In[144]:
 
-trainX.shape
-
-
-# In[58]:
-
-valX.shape[0]
+def feat(data,i):
+    return [1] + data[i].tolist()
 
 
-# In[62]:
+# In[145]:
 
-def feat1(i):
-    return [1] + trainX[i].tolist()
-def feat2(i):
-    return [1] + testX[i].tolist()
-def feat3(i):
-    return [1] + valX[i].tolist()
+trnX = np.array([feat(trainX,i) for i in range(trainX.shape[0])])/256.0
+tesX = np.array([feat(testX,i) for i in range(testX.shape[0])])/256.0
+vaX = np.array([feat(valX,i) for i in range(valX.shape[0])])/256.0
 
 
-# In[65]:
+# In[146]:
 
-trnX = np.array([feat1(i) for i in range(trainX.shape[0])])/256.0
-tesX = np.array([feat2(i) for i in range(testX.shape[0])])/256.0
-vaX = np.array([feat3(i) for i in range(valX.shape[0])])/256.0
+trnY = np.zeros((18000,10))
+tesY = np.zeros((2000,10))
+vaY = np.zeros((2000,10))
+for i in range(18000):
+    trnY[i, trainY[i]]=1;
+    if i<2000:
+        vaY[i, valY[i]]=1;
+        tesY[i, testY[i]]=1;
 
 
-# In[75]:
+# In[147]:
 
-w = np.random.rand(10, 785)/1000
+def softmax(x):
+    x=np.exp(x)
+    x=x/x.sum(axis=1)[:,None]
+    return x
 
 
-# In[165]:
+# In[148]:
 
-arr=np.zeros((18000, 10))
+def error(trnY, y):
+    er = 0    
+    for i in range(len(trnY)):
+        for k in range(10):
+            if(y[i, k] < 0.00001):
+                y[i, k] = 0.00001
+            elif(y[i, k] > 0.99999):
+                y[i, k] = 0.99999
+            er += trnY[i, k]*np.log(y[i, k])
+    return -1*er/len(trnY)
+
+
+# In[155]:
+
+y=np.zeros((18000, 10))
 su=np.zeros(18000)
+err=np.zeros((100, 3))
 
 
 # In[ ]:
@@ -62,34 +79,35 @@ su=np.zeros(18000)
 
 
 
-# In[ ]:
+# In[156]:
+
+lr=0.001
 
 
+# In[157]:
+
+w = (np.random.rand(10, 785)-0.5)/1000
 
 
-# In[ ]:
+# In[158]:
+
+for iter in range (100):
+    y=softmax(np.dot(trnX,np.transpose(w)))
+    err[iter,0]=error(trnY, y)
+    err[iter,1]=error(tesY, softmax(np.dot(tesX,np.transpose(w))))
+    err[iter,2]=error(vaY, softmax(np.dot(vaX,np.transpose(w))))
+    grad=np.dot(np.transpose(trnY-y), trnX)
+    w=w+lr*grad
+    
 
 
+# In[159]:
 
-
-# In[ ]:
-
-
-
-
-# In[166]:
-
-for iter in range (1):
-    arr=np.dot(w,np.transpose(trnX))
-    arr=np.exp(arr)
-    su=arr.sum(axis=0)
-    arr=arr/su
-        
-
-
-# In[168]:
-
-arr[0]
+plt.plot([x for x in range(0, len(err))], err[:,0], label = "train")
+plt.plot([x for x in range(0, len(err))], err[:,1], label = "test")
+plt.plot([x for x in range(0, len(err))], err[:,2], label = "validation")
+plt.legend(loc='upper right', shadow=True)
+plt.show()
 
 
 # In[ ]:
